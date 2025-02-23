999abe940c1675c0736c0168301d7989e360084e
mr-panther01 <adityasharma92006@gmail.com>
2025-02-24 01:07:25 +0530

    initialize React + TypeScript + Vite project with basic components and configurations

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import Alert from "./Components/alert";
+
+function App() {
+ 
+  return (
+    <div>
+      <Alert>
+        Hello <span> World</span>
+      </Alert>
+    </div>
+  );
+}
+
+export default App;
